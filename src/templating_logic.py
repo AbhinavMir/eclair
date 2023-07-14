6ff62475e0a9a4bd7acf3b46d2e0e7fa9937305e
Abhinav Srivastava <atg271@gmail.com>
2023-07-15 03:01:57 +0530

    wip

--- src/templating_logic.py
+++ src/templating_logic.py
@@ -6,82 +6,83 @@ import logging
 def pure_view_no_input_no_output(abi_function: ABI_class.ContractABI.FunctionABI):
     name = abi_function.name
     template = f"""
-def call_function_{name}(self) -> None:
-    return self.call_function('{name}')
-"""
+    def call_function_{name}(self) -> None:
+        return self.call_function('{name}')
+    """
     return template
 
 def pure_view_no_input_output(abi_function: ABI_class.ContractABI.FunctionABI):
     name = abi_function.name
     outputs = abi_function.outputs  
-    return_type = ", ".join(output.type for output in outputs)
+    return_type = ", ".join(output.pythonic_type for output in outputs)
     template = f"""
-def call_function_{name}(self) -> ({return_type}):
-    return self.call_function('{name}')
-"""
+    def call_function_{name}(self) -> ({return_type}):
+        return self.call_function('{name}')
+    """
     return template
 
 def pure_view_input_no_output(abi_function: ABI_class.ContractABI.FunctionABI):
     name = abi_function.name
     inputs = abi_function.inputs
-    input_type = ", ".join(input.type for input in inputs)
+    input_params = ", ".join(f"{input.name}: {input.pythonic_type}" for input in inputs)
     template = f"""
-def call_function_{name}(self, {input_type}) -> None:
-    return self.call_function('{name}', {input_type})
-"""
+    def call_function_{name}(self, {input_params}) -> None:
+        return self.call_function('{name}', {input_params})
+    """
     return template
 
 def pure_view_input_output(abi_function: ABI_class.ContractABI.FunctionABI):
     name = abi_function.name
     inputs = abi_function.inputs
-    input_type = ", ".join(input.type for input in inputs)
+    input_params = ", ".join(f"{input.name}: {input.pythonic_type}" for input in inputs)
     outputs = abi_function.outputs
-    return_type = ", ".join(output.type for output in outputs)
+    return_type = ", ".join(output.pythonic_type for output in outputs)
     template = f"""
-def call_function_{name}(self, {input_type}) -> ({return_type}):
-    return self.call_function('{name}', {input_type})
-"""
+    def call_function_{name}(self, {input_params}) -> ({return_type}):
+        return self.call_function('{name}', {input_params})
+    """
     return template
 
 def exec_no_input_no_output(abi_function: ABI_class.ContractABI.FunctionABI):
     name = abi_function.name
     template = f"""
-def execute_transaction_{name}(self) -> None:
-    return self.execute_transaction('{name}')
-"""
+    def execute_transaction_{name}(self) -> None:
+        return self.execute_transaction('{name}')
+    """
     return template
 
 def exec_no_input_output(abi_function: ABI_class.ContractABI.FunctionABI):
     name = abi_function.name
     outputs = abi_function.outputs
-    return_type = ", ".join(output.type for output in outputs)
+    return_type = ", ".join(output.pythonic_type for output in outputs)
     template = f"""
-def execute_transaction_{name}(self) -> ({return_type}):
-    return self.execute_transaction('{name}')
-"""
+    def execute_transaction_{name}(self) -> ({return_type}):
+        return self.execute_transaction('{name}')
+    """
     return template
 
 def exec_input_no_output(abi_function: ABI_class.ContractABI.FunctionABI):
     name = abi_function.name
     inputs = abi_function.inputs
-    input_type = ", ".join(input.type for input in inputs)
+    input_params = ", ".join(f"{input.name}: {input.pythonic_type}" for input in inputs)
     template = f"""
-def execute_transaction_{name}(self, {input_type}) -> None:
-    return self.execute_transaction('{name}', {input_type})
-"""
+    def execute_transaction_{name}(self, {input_params}) -> None:
+        return self.execute_transaction('{name}', {input_params})
+    """
     return template
 
 def exec_input_output(abi_function: ABI_class.ContractABI.FunctionABI):
     name = abi_function.name
     inputs = abi_function.inputs
-    input_type = ", ".join(input.type for input in inputs)
+    input_params = ", ".join(f"{input.name}: {input.pythonic_type}" for input in inputs)
     outputs = abi_function.outputs
-    return_type = ", ".join(output.type for output in outputs)
+    return_type = ", ".join(output.pythonic_type for output in outputs)
     template = f"""
-def execute_transaction_{name}(self, {input_type}) -> ({return_type}):
-    return self.execute_transaction('{name}', {input_type})
-"""
+    def execute_transaction_{name}(self, {input_params}) -> ({return_type}):
+        return self.execute_transaction('{name}', {input_params})
+    """
     return template
+
 
 def create_function(abi_function: ABI_class.ContractABI.FunctionABI):
     logging.info("Creating function %s", abi_function.name)
@@ -119,10 +120,27 @@ def create_function(abi_function: ABI_class.ContractABI.FunctionABI):
     logging.info("%s: %s", name, template)
     return t
 
+def create_class(abi: ABI_class.ContractABI):
+    name = abi.name
+    functions = abi.functions
+    constructor = abi.constructor
+    fallback = abi.fallback
+    receive = abi.receive
+    template = f"""
+class contract_{name}_class:
+""" 
+    if constructor:
+        template += create_function(constructor)
+    if fallback:
+        template += create_function(fallback)
+    if receive:
+        template += create_function(receive)
+    
+    for function in functions:
+        template += create_function(function)
+    return template
+
 abi = ABI_class.ContractABI(
     "/Users/abhinavmir/Desktop/Code/eclair/artifacts/contracts/payer.sol/PayeeContract.json")
-for func in abi.functions:
-    try:
-        print(create_function(func))
-    except Exception as e:
-        print(e)
+
+print(create_class(abi))
